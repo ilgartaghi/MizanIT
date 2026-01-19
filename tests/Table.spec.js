0d5006b2580f40a71806aaadfa890ad388f9af7a
Ilgar <ilgartaghi@gmail.com>
2026-01-18 21:02:29 -0500

    your message

--- tests/Table.spec.js
+++ tests/Table.spec.js
@@ -1,9 +1,28 @@
 const {test, expect}=require('@playwright/test')
 
-test("simple table",async ({page})=>{
+test("handling table",async ({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/');
 
+    const table = await page.locator('#productTable');
+
+    // 1. total # of rows and columns
+    const columns = await table.locator('thead tr th');
+    console.log('# of columns:', await columns.count());
+
+    const rows = await table.locator('tbody tr');
+    console.log('Number of rows:', await rows.count());
+
+
+    expect(await columns.count()).toBe(4);
+    expect(await rows.count()).toBe(5);
+
+    //2. Select checkbox for product 4
+
+    await rows.filter({
+        has: page.locator('td');
+        hasText: 'Product 4'
+    })
 
 
 
