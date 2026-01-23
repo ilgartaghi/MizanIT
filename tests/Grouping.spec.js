c3e96efff9ab26784c50ad89000479d27ef3358f
itaghiyev <110480452+itaghiyev@users.noreply.github.com>
2026-01-23 09:00:17 -0500

    Grouping

---
+++ tests/Grouping.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+////////////////////////////////////////////////////
+test.beforeAll(async () => {
+    console.log('This is beforeAll Hook....')
+})
+
+test.afterAll(async () => {
+    console.log('This is afterAll Hook....')
+})
+/////////////////////////////////
+test.beforeEach(async () => {
+    console.log('This is beforeEach Hook....')
+})
+
+test.afterEach(async () => {
+    console.log('This is afterEach Hook....')
+})
+////////////////////////////////////////////////////
+test.describe('Group1',() => {
+
+    test('Test1',async({page}) => {
+    console.log('This is test 1....')
+})
+
+test('Test2',async({page}) => {
+    console.log('This is test 2....')
+})
+/////////////////////////////////
+})
+
+test.describe('Group2',()=>{
+
+    test('Test3',async({page})=>{
+    console.log('This is test 3....')
+})
+
+test('Test4',async({page})=>{
+    console.log('This is test 4....')
+})
+
+})
+
+
+
