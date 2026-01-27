a82744678a8c6c73d55704f333c9cfd39767427e
itaghiyev <110480452+itaghiyev@users.noreply.github.com>
2026-01-27 09:04:28 -0500

    Page Object model

---
+++ tests/Pomtest.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pages/LoginPage';
+import { HomePage } from './pages/HomePage';
+import { CartPage } from './pages/CartPage';
+
+test('test', async ({ page }) => {
+
+  ////////   without pom   ///////////////
+  /*
+  await page.goto('https://www.demoblaze.com/index.html');
+  await page.locator('#login2').click();
+  await page.locator('#loginusername').fill('pavanol');
+  await page.locator('#loginpassword').fill('test@123');
+  await page.locator('//button[normalize-space()="Log in"]').click();
+ */
+
+  ///////////   with pom    ////////////
+  //Login Page
+  const login = new LoginPage(page)
+  await login.gotoLoginPage()
+  await login.login('pavanol', 'test@123')
+  await page.waitForTimeout(3000)
+
+  
+});
