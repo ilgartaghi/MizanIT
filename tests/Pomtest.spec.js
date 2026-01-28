91828d8f684c35cebad1d8012ab4bf8995f51a93
itaghiyev <110480452+itaghiyev@users.noreply.github.com>
2026-01-28 08:44:14 -0500

    Page Object Model 2

--- tests/Pomtest.spec.js
+++ tests/Pomtest.spec.js
@@ -21,5 +21,17 @@ test('test', async ({ page }) => {
   await login.login('pavanol', 'test@123')
   await page.waitForTimeout(3000)
 
+  //Home Page
+  const home = new HomePage(page)
+  await home.addProductToCart('Nexus 6')
+  await page.waitForTimeout(3000)
+  await home.gotoCart();
+
+  //Cart Page
+  const cart = new CartPage(page)
+  await page.waitForTimeout(3000)
+  const status=await cart.checkProductInCart('Nexus 6');
+  expect(await status).toBe(true);
+
   
 });
